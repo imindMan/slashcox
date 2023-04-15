65abccc77b173419f5fed5e079c17f36a5b34c69
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-04-15 09:23:35 +0000

    fixed some stuff in config

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 ### CONFIG (embed in the future) ###
-# 
+#
 # Mainly migrating from discox. Config class supports setting inside the .env file (for security & privacy purpose)
 #
 ####################################
@@ -13,32 +13,42 @@ import datetime
 import os
 from typing import List
 
+#os.getenv returns an empty string if the value is empty so it doesn't use the second argument
+def getenv(name: str, other: str=None) -> any:
+    """Gets an environment variable
+    [Args]:
+        name (str): The name of the env variable
+        other (str): Will return if name is empty or None
+    """
+    env = os.getenv(name)
+    return env if env or other is None else other
+
 ### CONFIG DEFINITION ###
 class Config:
-    token: str = os.getenv("SLASHCOX_TOKEN")  # bot token
+    token: str = getenv("SLASHCOX_TOKEN")  # bot token
     report_channel_id: int = int(
-        os.getenv("SLASHCOX_REPORT_ID", 1064539181193375784)
+        os.getenv("SLASHCOX_REPORT_ID", "1064539181193375784")
     )  # mod role id
     mod_role_id: List[int] = [
-        int(x) for x in os.getenv("SLASHCOX_MOD_ROLE_ID", "0").split(",")
+        int(x) for x in getenv("SLASHCOX_MOD_ROLE_ID", "0").split(",")
     ]  # mod role id
-    temp_channel: int = int(os.getenv("SLASHCOX_TEMP_CHANNEL", "0"))  # temp channel id
-    channel_id: str = os.getenv(
+    temp_channel: int = int(getenv("SLASHCOX_TEMP_CHANNEL", "0"))  # temp channel id
+    channel_id: str = getenv(
         "SLASHCOX_CHANNEL_ID", "UCCFVFyadjMuaR5O89yRToew"
     )  # channel id
-    role_channel: int = int(os.getenv("SLASHCOX_ROLE_CHANNEL", "0"))  # role channel
+    role_channel: int = int(getenv("SLASHCOX_ROLE_CHANNEL", "0"))  # role channel
     youtube_announcement_id: int = int(
-        os.getenv("SLASHCOX_YOUTUBE_ANNOUNCEMENT_ID", 1056990617357521009)
+        getenv("SLASHCOX_YOUTUBE_ANNOUNCEMENT_ID", "1056990617357521009")
     )  # youtube announcement id
-    mysql_host: str = os.getenv("SLASHCOX_MYSQL_HOST", "localhost")
-    mysql_port: int = int(os.getenv("SLASHCOX_MYSQL_PORT", 3306))
-    mysql_user: str = os.getenv("SLASHCOX_MYSQL_USER", "root")
-    mysql_password: str = os.getenv("SLASHCOX_MYSQL_PASSWORD", "")  # recommendaton :tf:
-    mysql_database: str = os.getenv(
+    mysql_host: str = getenv("SLASHCOX_MYSQL_HOST", "localhost")
+    mysql_port: int = int(getenv("SLASHCOX_MYSQL_PORT", "3306"))
+    mysql_user: str = getenv("SLASHCOX_MYSQL_USER", "root")
+    mysql_password: str = getenv("SLASHCOX_MYSQL_PASSWORD", "")  # recommendaton :tf:
+    mysql_database: str = getenv(
         "SLASHCOX_MYSQL_DATABASE", "discox"
-    )  
+    )
     starboard_channel: int = int(
-        os.getenv("SLASHCOX_STARBOARD_CHANNEL", "0")
+        getenv("SLASHCOX_STARBOARD_CHANNEL", "0")
     )  # starboard channel
-    server_id: int = int(os.getenv("SLASHCOX_SERVER_ID", "1032277950416035930")) # imindworld server id
+    server_id: int = int(getenv("SLASHCOX_SERVER_ID", "1032277950416035930")) # imindworld server id
 
