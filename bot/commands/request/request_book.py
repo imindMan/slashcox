3fc9b8dfdcc4ad494573f01d301e492e62991892
imindMan <khanhdonglethanhtong7b2@gmail.com>
2023-06-12 13:57:40 +0700

    Request implementation finished. Jump to the moderators implementation

---
+++ bot/commands/request/request_book.py
@@ -0,0 +1,117 @@
+from bot.base import BaseCommand
+from bot.config import Config, Embed
+
+
+class cmd(BaseCommand):
+    """
+    INFO: This command is one of the support commands of the request command. It is used to show the request database.
+    HOW TO USE:
+
+    - Type in `v!req request_book <number_id_of_the_request>`
+    The number_id here is just basically the order number of the request.
+    If you want to show all of the database, the number_id will be 0
+
+    For example:
+        `v!req request_book 1` will show the request with the number_id: 1
+        `v!req request_book 0` will show all of the request database
+    """
+
+    name = "request_book"
+    description = "Show the request followed by the id. "
+
+    def display_request(self, row: tuple) -> str:
+        """
+
+        INFO: this method is used to display the request in the request table.
+        After cursor.fetchone(), it will return a tuple which represents the request in the database. To make it easier to read, this function will reformat the tuple and then print out the formatted string.
+
+        HOW IT WORKS:
+        The tuple will look like this:
+            (<number_id>, <author_information>, <title>, <description>, <upvote>, <downvote>, <pending_close>)
+        For example:
+            (1, "imindMan#8536", 'Hello', 'Hello guys', 0, 0, 0)
+        The bot will then use this tuple, reformat it to a string, then return it
+        THe final result will look like this (based on the example):
+            1. 'Hello' by imindMan#8536
+               Vote count: U:0 - D:0 - G:0
+               Pending close: 0
+        """
+        # implement number_id
+        number_id = row[0]
+
+        # implement member_id
+        member_id = row[1]
+
+        # implement title
+        title = row[2]
+
+        # implement upvote
+        upvote = row[4]
+        # downvote
+        downvote = row[5]
+
+        # Pending_close
+        pending_close = row[6]
+        final = f"{number_id}. '{title}' by {member_id}\nVote count: U:{upvote} - D:{downvote} - G:{int(upvote) - int(downvote)}\nPending close: {pending_close}"
+        return final
+
+    async def execute(self, interaction, number_id:int) -> None:
+        """
+        MAIN EXECUTION:
+            After the user type in the command, the bot will check if the arguments[0] (the first argument) is 0 or another number. If it's 0, then the bot will select the request table in the database and show all of them in a specific format that looks like this:
+                List of all the requests:
+                <number_id>. <title> by <member_id>.
+                    Vote count: U:<upvote> - D:<downvote> - G:<upvote> - <downvote>
+                    Pending close: <pending_close>
+                    ...
+            If it's another number, then the bot will select that row with that number_id. If the row doesn't exist, it throws error. Otherwise, it will show up like this:
+                Show the information of the row with the given number_id: <number_id>
+                    <number_id>. <title> by <member_id>.
+                        Vote count: U:<upvote> - D:<downvote>
+                        Pending close: <pending_close>
+                    Description:
+                        <description>
+        """
+
+        # in case if arguments[0] == "0"
+        if number_id == 0:
+            # Select from the database
+            rows = await self.db.raw_exec_select("SELECT * FROM request")
+            if len(rows) == 0:
+                # Throw error if the bot doesn't find anything in the database
+                embed = Embed(title="Couldn't find anything in the database")
+                embed.set_color("red")
+                await interaction.response.send_message(embed=embed)
+            else:
+                # Display as normally
+                final_message = ""
+                for row in rows:
+                    # Looping through every elements in the database
+                    final = self.display_request(row)
+                    final_message += f"{final}\n"
+                # Print all of them
+                embed = Embed(
+                    title="List of all the requests:", description=final_message
+                )
+                await interaction.response.send_message(embed=embed)
+        else:
+    
+
+            row = await self.db.raw_exec_select(
+                f"SELECT * FROM request WHERE Number_id = ?", (number_id,)
+            )
+            if len(row) == 0:
+                embed = Embed(title="Invalid number_id")
+                embed.set_color("red")
+                await interaction.response.send_message(embed=embed)
+                return
+
+            row = row[0]
+            # Display it
+            final_message = self.display_request(row)
+            final_message += f"\nDescription: ```{row[3]}```"
+            embed = Embed(
+                title=f"Show the information of the row with the given number_id: `{number_id}`",
+                description=final_message,
+            )
+            await interaction.response.send_message(embed=embed)
---
+++ bot/commands/request/request_status.py
@@ -0,0 +1,100 @@
+
+from bot.base import BaseCommand
+from bot.config import Config, Embed
+
+
+class cmd(BaseCommand):
+    """
+    INFO: this command shows the status of the request database
+    The status of the request database will look like this:
+        <List-of-the-requests-haven't-started-voting-yet>
+        <List-of-the-pending-close-requests>
+
+    """
+
+    name = "request_status"
+    description = "Show the current status of the request database"
+
+    def display_request(self, row: tuple) -> str:
+        """
+
+        INFO: this method is used to display the request in the request table.
+        After cursor.fetchone(), it will return a tuple which represents the request in the database. To make it easier to read, this function will reformat the tuple and then print out the formatted string.
+
+        HOW IT WORKS:
+        The tuple will look like this:
+            (<number_id>, <author_information>, <title>, <description>, <upvote>, <downvote>, <pending_close>)
+        For example:
+            (1, "imindMan#8536", 'Hello', 'Hello guys', 0, 0, 0)
+        The bot will then use this tuple, reformat it to a string, then return it
+        THe final result will look like this (based on the example):
+            1. 'Hello' by imindMan#8536
+
+        """
+        # implement number_id
+        number_id = row[0]
+
+        # implement member_id
+        member_id = row[1]
+
+        # implement title
+        title = row[2]
+
+        final = f"{number_id}. '{title}' by {member_id}"
+        return final
+
+    async def execute(self, interaction) -> None:
+        """
+        HOW IT WORKS:
+            The bot first checks all of the requests and collect these types of data:
+                    <List-of-the-requests-haven't-started-voting-yet>
+                    <List-of-the-pending-close-requests>
+                    <Vote-count-for-the-requests>
+            After collecting all of them, it just prints out the result and that's it!
+
+            <Requests-haven't-started-voting-yet>:
+                Looping for all of the requests, then tracing back all the requests with the upvote and downvote = 0.
+                Return a list of number_id.
+            <Pending_close-requests>
+                Looping for all of the requests, then tracing back all the requests with the pending_close = 1
+                Also return a list of number_id
+
+        """
+
+        # getting all of the requests
+        requests = await self.db.raw_exec_select("SELECT * FROM request")
+        # initialize all the needed variable (self-described names also)
+
+        requests_havent_started_voting = []
+        requests_havent_started_voting_count = 0
+        requests_pending_close = []
+        requests_pending_close_count = 0
+
+        # looping and filtering every information
+
+        for request in requests:
+            # checking if the request has upvote and downvote = 0
+
+            if int(request[4]) == int(request[5]) == 0:
+                requests_havent_started_voting.append(int(request[0]))
+                requests_havent_started_voting_count += 1
+            # checking if the request has pending_close = 1
+            if int(request[6]) == 1:
+                requests_pending_close.append(int(request[0]))
+                requests_pending_close_count += 1
+
+        # after that, print out every thing
+
+        requests_havent_started_voting_list = ""
+        requests_pending_close_list = ""
+
+        for i in requests_havent_started_voting:
+            requests_havent_started_voting_list += (
+                self.display_request(requests[i - 1]) + "\n"
+            )
+        for i in requests_pending_close:
+            requests_pending_close_list += self.display_request(requests[i - 1]) + "\n"
+
+        message_to_show = f"Total amount of requests haven't started voting yet: {requests_havent_started_voting_count}. List all of them: \n{requests_havent_started_voting_list}\nTotal amount of requests pending close: {requests_pending_close_count}. List all of them: \n{requests_pending_close_list}"
+        requests_pending_close_embed = Embed(title=message_to_show)
+        await interaction.response.send_message(embed=requests_pending_close_embed)
