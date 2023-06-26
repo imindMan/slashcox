15737fc2cc97a45e8991cc9341e6a94851e790e0
Code beautifier <imindMan@users.noreply.github.com>
2023-06-26 02:24:22 +0000

    Beautified

--- bot/commands/fetch/updatefetch.py
+++ bot/commands/fetch/updatefetch.py
@@ -30,14 +30,37 @@ class cmd(BaseCommand):
         git: str = None,
         memory: str = None,
     ) -> None:
-        arguments = [image, distro, kernel, terminal, editor, shell, de_wm, bar, resolution, display_protocol, gtk_theme, gtk_icon_theme, cpu, gpu, description, dotfiles, git, memory]
+        arguments = [
+            image,
+            distro,
+            kernel,
+            terminal,
+            editor,
+            shell,
+            de_wm,
+            bar,
+            resolution,
+            display_protocol,
+            gtk_theme,
+            gtk_icon_theme,
+            cpu,
+            gpu,
+            description,
+            dotfiles,
+            git,
+            memory,
+        ]
 
         for i in range(len(arguments)):
             if arguments[i] is not None:
                 col = await self.db.raw_exec_select(
-                        "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE ORDINAL_POSITION = ? AND TABLE_NAME = 'fetch';",(i + 2,)
-                        )
+                    "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE ORDINAL_POSITION = ? AND TABLE_NAME = 'fetch';",
+                    (i + 2,),
+                )
 
-                await self.db.raw_exec_commit(f"UPDATE slashcox.fetch SET {col[0][0]} = ? WHERE user = ?;", (arguments[i], interaction.user))
-        embed = Embed(title = "Successfully update the fetch")
-        await interaction.response.send_message(embed = embed)
+                await self.db.raw_exec_commit(
+                    f"UPDATE slashcox.fetch SET {col[0][0]} = ? WHERE user = ?;",
+                    (arguments[i], interaction.user),
+                )
+        embed = Embed(title="Successfully update the fetch")
+        await interaction.response.send_message(embed=embed)
