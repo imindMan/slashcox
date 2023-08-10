52b2a17d4e4b67b19e6bd6bf27c4fde228434eb9
Code beautifier <imindMan@users.noreply.github.com>
2023-08-10 14:28:42 +0000

    Beautified

--- bot/commands/changePresence/status.py
+++ bot/commands/changePresence/status.py
@@ -1,8 +1,10 @@
+from typing import Literal
+
 import discord
 
 from bot.base import BaseCommand
 from bot.config import Config, Embed
-from typing import Literal
+
 
 class cmd(BaseCommand):
     """A discord command instance."""
@@ -10,7 +12,11 @@ class cmd(BaseCommand):
     name = "status"
     description = "Changes the bot's status."
 
-    async def execute(self, interaction, _type:Literal["online", "idle", "do_not_disturb", "invisible"]) -> None:
+    async def execute(
+        self,
+        interaction,
+        _type: Literal["online", "idle", "do_not_disturb", "invisible"],
+    ) -> None:
         match _type:
             case "online":
                 status = discord.Status.online
@@ -20,7 +26,7 @@ class cmd(BaseCommand):
                 status = discord.Status.do_not_disturb
             case "invisible":
                 status = discord.Status.invisible
-            
+
         await self.bot.change_presence(
             status=status, activity=self.bot.current_activity
         )
@@ -29,6 +35,7 @@ class cmd(BaseCommand):
             title="Status changed", description=f"Succesfully changed status to {type}"
         )
         await interaction.response.send_message(embed=embed)
+
     def check_permissions(self, interaction: discord.Interaction) -> bool:
         # Check for a specific role in the member
         return any([i.id == Config().mod_role_id for i in interaction.user.roles])
